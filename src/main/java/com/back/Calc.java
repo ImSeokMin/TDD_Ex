738d5e7beb0d5e02b412c135bc341708c36a9440
ImSeokMin <imseokmin01@gmail.com>
2025-05-30 14:51:02 +0900

    t1, green

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,4 +1,10 @@
 package com.back;
 
 public class Calc {
+    public static int run(String expression){
+        if(expression.equals("1 + 1")){
+            return 2;
+        }
+        return 0;
+    }
 }
