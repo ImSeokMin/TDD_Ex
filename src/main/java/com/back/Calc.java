44fb5d1114410a9ebfc93fc1b720c8ed5db9282f
ImSeokMin <imseokmin01@gmail.com>
2025-05-30 15:36:08 +0900

    t3, refactor

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,13 +2,12 @@ package com.back;
 
 public class Calc {
     public static int run(String expression){
-        if(expression.equals("1 + 1")){
-            return 2;
-        } else if (expression.equals("2 + 1")) {
-            return 3;
-        } else if (expression.equals("2 + 2")) {
-            return 4;
-        }
-        return 0;
+        String[] expressionBits = expression.split("\\+");
+
+        int num1 = Integer.parseInt(expressionBits[0].trim());
+        int num2 = Integer.parseInt(expressionBits[1].trim());
+        int sum = num1 + num2;
+        return sum;
     }
 }
+
